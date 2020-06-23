1a1e15c3834a651c5a5a0fc9c2594aa6f576d610
Jacob Haff <jacobhaff2@gmail.com>
2020-06-23 17:00:48 -0400

    Initial Commit

---
+++ zat-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zat-chat
+//
+//  Created by Jacob Haff on 6/23/20.
+//  Copyright © 2020 Jacob Haff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
