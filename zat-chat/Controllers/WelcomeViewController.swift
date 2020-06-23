7a1260aa8355861706348b5dfac306645431eebb
Jacob Haff <jacobhaff2@gmail.com>
2020-06-23 19:11:13 -0400

    Animate welcome screen

--- zat-chat/Controllers/WelcomeViewController.swift
+++ zat-chat/Controllers/WelcomeViewController.swift
@@ -15,6 +15,16 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.text = ""
+        var letterIndex = 0.0
+        
+        for letter in "⚡️ZatChat" {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * letterIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            
+            letterIndex += 1
+        }
        
     }
     
